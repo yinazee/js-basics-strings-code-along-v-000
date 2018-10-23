93ff633925b10928c3662648e39755e7ce1eedbb
Yina Zhu <yinagit@gmail.com>
2018-10-23 21:10:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
